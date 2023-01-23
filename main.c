53b52cd79251abf2b4e4ffe7a3e32ac33dce0402
jboeddeker <jboeddeker@gmx.de>
2023-01-23 15:08:23 +0100

    added min and max duration filter

--- main.c
+++ main.c
@@ -6,10 +6,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/queue.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+
+#include <unistd.h>
+
 #include <curl/curl.h>
 
 #include "args.h"
